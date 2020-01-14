bc3698dd7a5b1dd316fff9fd01941a70f6263325
SoulMonk <fsoulmonk@gmail.com>
2020-01-14 16:49:18 +0200

    updated gateway

--- gateway/index.js
+++ gateway/index.js
@@ -3,6 +3,15 @@
 const fastify = require('fastify')({ logger: true })
 const proxy = require('fastify-http-proxy')
 
+fastify.register(require('fastify-cors'), {
+  origin: [
+    'http://localhost:4541', // ng serve - dev
+    'http://localhost:3500' // dev separated
+  ]
+})
+
+// note if api returns 404, proxy goes to the other route
+
 fastify.register(proxy, {
   upstream: 'http://localhost:3030',
   prefix: '/auth'
@@ -17,5 +26,19 @@ fastify.register(proxy, {
   upstream: 'http://localhost:3035',
   prefix: '/time-tracker'
 })
+
+// 'http://localhost:3000/api/notes' from origin 'http://localhost:3500'
+// TODO using old server, before migrate
+fastify.register(proxy, {
+  upstream: 'http://localhost:4540',
+  prefix: '/api',
+  rewritePrefix: '/api'
+})
+
+// TODO web app
+// fastify.register(proxy, {
+//   upstream: 'http://localhost:3500',
+//   prefix: '/'
+// })
 
 fastify.listen(3000)
