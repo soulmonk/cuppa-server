efdd12c796d73f7c6684d66f758bf5a5fa51b0c9
SoulMonk <fsoulmonk@gmail.com>
2020-02-10 08:00:56 +0200

    to history

--- finance-stats/test/graphql/query.test.js
+++ finance-stats/test/graphql/query.test.js
@@ -3,9 +3,26 @@
 const { test } = require('tap')
 const { build } = require('../helper')
 
+const sinon = require('sinon')
+
 // TODO dependency on db
 test('get all transactions', async t => {
   const app = await build(t)
+  await app.ready()
+
+  const clientQueryStub = sinon.stub()
+
+  const clientStub = {
+    query: clientQueryStub,
+    release: sinon.stub().returns()
+  }
+  const pgClientStub = sinon.stub(app.pg, 'connect')
+  pgClientStub.resolves(clientStub)
+  const pgQueryStub = sinon.stub(app.pg, 'query')
+
+  clientQueryStub.resolves({ rows: [] })
+  pgQueryStub.resolves({ rows: [] })
+
   const response = await app.inject({
     method: 'POST',
     url: '/graphql',
@@ -18,9 +35,14 @@ test('get all transactions', async t => {
     }
   })
 
+  console.log('query.test.js::::35 >>>', response.payload)
+
   t.strictEqual(response.statusCode, 200)
   const payload = JSON.parse(response.payload)
   t.type(payload.data.transactions.length, 'number')
+
+  pgClientStub.restore()
+  pgQueryStub.restore()
 })
 
 test('get transaction types', async t => {
