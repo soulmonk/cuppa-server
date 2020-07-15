6927cdf86398234b738a935103d67ef22ba83961
SoulMonk <fsoulmonk@gmail.com>
2020-07-15 18:36:46 +0300

    updated package.json removed fastify-jwt as related library

--- finance-stats/plugins/jwt.js
+++ finance-stats/plugins/jwt.js
@@ -1,3 +1,42 @@
 'use strict'
 
-module.exports = require('../../library/fastify-plugins/jwt')
+const fp = require('fastify-plugin')
+const jwt = require('fastify-jwt')
+
+// todo as repository ?, copied because docker could not copy out of context
+async function fastifyJWT (fastify, opts) {
+
+  fastify.decorateRequest('user', null)
+
+  fastify.register(jwt, {
+    secret: opts.jwt.secret
+  })
+
+  async function authenticate (request, reply) {
+    const error = (err) => {
+      reply.log.error(err)
+      reply.code(401).send({ message: 'Could not authenticate' })
+      return false
+    }
+    try {
+      const data = await request.jwtVerify()
+      if (!data.id) {
+        return error(new Error('wrong payload'))
+      }
+      request.user = {
+        id: data.id
+      }
+    } catch (err) {
+      return error(err)
+    }
+    return true
+  }
+
+  fastify.decorate('authenticate', authenticate)
+
+  if (opts.jwt.addHook) {
+    fastify.addHook('onRequest', authenticate)
+  }
+}
+
+module.exports = fp(fastifyJWT)
