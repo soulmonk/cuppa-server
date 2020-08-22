d22878c5ba7dc9494c2a71a726001f1bb17a6085
SoulMonk <fsoulmonk@gmail.com>
2020-08-22 08:07:40 +0300

    created migration script

--- finance-stats/db/migration/index.js
+++ finance-stats/db/migration/index.js
@@ -2,15 +2,89 @@ const fs = require('fs').promises
 const path = require('path')
 
 async function initTable (db) {
+  let query = `SELECT COUNT(1) as count
+               FROM migrations;`
 
+  try {
+    const res = await db.query(query)
+    return +res.rows[0].count
+  } catch (err) {
+    if (err.code !== '42P01') {
+      throw err
+    }
+  }
+
+  query = `CREATE TABLE migrations
+           (
+               id         serial not null
+                   constraint migrations_pk
+                       primary key,
+               name       varchar(255),
+               created_at timestamp default now()
+           );
+  `
+  await db.query(query)
+
+  return 0
+}
+
+async function loadMigration (db, count) {
+  if (!count) {
+    return []
+  }
+  const { rows } = db.query(`SELECT name
+                             FROM migrations`)
+
+  return rows.map(({ name }) => name)
+}
+
+async function getMigrationList (storedMigrations) {
+  const dir = await fs.readdir(__dirname)
+
+  const storedMigrationsMap = storedMigrations.reduce((acc, name) => (acc[name] = 1, acc), {})
+  const result = []
+  for (let file of dir) {
+    if (file === 'index.js' || storedMigrationsMap[file]) {
+      continue
+    }
+
+    result.push({
+      name: file,
+      data: await fs.readFile(path.join(__dirname, file), 'utf8')
+    })
+  }
+  return result
+}
+
+async function applyMigration (db, migrationFiles) {
+  for (let file of migrationFiles) {
+    console.log('Start process data:', file.name)
+    console.log('Run query:\n', file.data)
+    await db.query(file.data)
+    await create(db, file.name)
+  }
 }
 
 async function migration (db) {
-  await initTable(db)
+  const count = await initTable(db)
+  console.log('Stored migrations', count)
+  const storedMigrations = await loadMigration(db, count)
+  const migrationData = await getMigrationList(storedMigrations)
 
-  const sql = await fs.readFile(path.join(__dirname, '1581120000000_init.sql'), 'utf8')
+  console.log('New migrations: ', migrationData.length)
 
-  await db.query(sql)
+  if (!migrationData.length) {
+    return
+  }
+
+  return applyMigration(db, migrationData)
+}
+
+async function create(db, name) {
+    const query  = `INSERT INTO migrations (name, created_at) VALUES ($1, now()) RETURNING id`
+
+    const result = await db.query(query, [name]);
+    console.log('Created new record:', result.rows[0].id);
 }
 
 module.exports = {
