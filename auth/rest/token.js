64485bbe950c4fdcd76da408be0cd891ca55347f
SoulMonk <fsoulmonk@gmail.com>
2020-08-22 09:26:44 +0300

    small fix

--- auth/rest/token.js
+++ auth/rest/token.js
@@ -76,11 +76,13 @@ async function tokenService (fastify, opts) {
 
     const user = await userRepository.getUserByName(username)
     if (!user) {
+      console.log('token.js::onGetToken::79 >>>', '')
       return error()
     }
 
     const validPwd = await userRepository.checkPassword(password, user.password)
     if (!validPwd) {
+      console.log('token.js::onGetToken::84 >>>', '')
       return error()
     }
 
